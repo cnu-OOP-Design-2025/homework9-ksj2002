64ffc2fa4d3dbf448c0d5285262e8e1ea17ce989
ksj2002 <ksj0204007@gmail.com>
2025-11-20 10:26:36 +0900

    과제 제출

--- MyVector.cpp
+++ MyVector.cpp
@@ -5,13 +5,56 @@ using namespace std;
 
 template<typename T>
 class MyVector {
-/* TODO */
+private:
+    T*data;
+    size_t capacity;
+    size_t length;
 
+    void resize(){
+        size_t oldCap = capacity;
+        capacity*=2;
+        cout << "Resize: " << oldCap << " -> " << capacity << endl;
 
+        T*newData = new T[capacity];
+        for (size_t i=0; i < length; i ++)
+            newData[i] = data[i];
 
+        delete[] data;
+        data = newData;
+    }
 
+    public:
+        MyVector(){
+            capacity = 2;
+            length = 0;
+            data = new T[capacity];
+    }
 
+    ~MyVector(){
+        delete[] data;
+    }
 
+    void push_back(const T& value){
+        if(length == capacity)
+            resize();
+        data[length++] = value;
+    }
+
+    T pop_back(){
+        return data[--length];
+    }
+
+    size_t size() const{
+        return length;
+    }
+
+    T& operator[](size_t index){
+        return data[index];
+    }
+
+    const T& operator[](size_t index) const{
+        return data[index];
+    }
 };
 
 template<typename T>
